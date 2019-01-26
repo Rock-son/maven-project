4daa07d6ceaad0cb047f9efec4b5fc1176332f97
Rok Zigon <roky.zz@gmail.com>
2019-01-26 15:19:37 +0100

    fix/greetr func tags

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,8 +14,8 @@ public class Greeter {
   
   /**
    * 
-   * @param someone
-   * @return
+   * @param someone String
+   * @return String
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
